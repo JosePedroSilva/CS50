51d920c9d4510a3da5ddd4ea8a7e8379faddb8c8
JosePedro <josepedro88@live.com.pt>
2020-05-26 12:31:57 +0100

    week 1 lecture

---
+++ Week1/counter.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+    for(int i = 1; i < 21; i++)
+    {
+        printf("Count:%i\n", i);
+
+    }
+}
---
+++ Week1/int.c
@@ -0,0 +1,7 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+    int age = get_int("What's your age?\n");
+    printf ("You are at least %i days old.\n", age * 365);
+}
